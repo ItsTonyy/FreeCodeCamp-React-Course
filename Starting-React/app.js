9be10ef9cdc4093be53196eae9206743b1fa0cd5
Tony <tonytnportela@outlook.com>
2023-05-19 10:38:56 -0300

    Updates

--- Starting-React/app.js
+++ Starting-React/app.js
@@ -1,1 +1,53 @@
-ReactDOM.render(<h1>Hello, React!</h1>, document.querySelector('#root'))
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src='/images/react-logo.png' width='90' height='80' />
+      </nav>
+
+      <h1>Reasons I'm excited to learn React</h1>
+    </header>
+  )
+}
+
+function Main() {
+  return (
+    <main>
+      <ol>
+        <li>It's a popular library, so you'll be able to fit in with the popular kids.</li>
+
+        <br></br>
+
+        <li>I'm more likely to get a job if I know react</li>
+      </ol>  
+
+      <br></br>
+      </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      © 2023 TerraNova Development. All rights reserverd.
+    </footer>
+  )
+}
+
+
+
+function Page() { 
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />  
+    </div>
+  )
+}
+
+ReactDOM.render(
+
+  <Page />,
+  document.getElementById('root')
+)
