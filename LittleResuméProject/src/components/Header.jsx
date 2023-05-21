1fdb46f8fb8226333a526b5ef0281311aaa0efe4
Tony <tonytnportela@outlook.com>
2023-05-20 23:58:50 -0300

    meh

---
+++ LittleResuméProject/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  <div>
+    <img src="/public/TonyPhoto.jpg" alt="My-Image" />
+    <h1>Tony Nova</h1>
+    <h3>FullStack Developer</h3>
+    <p>tonynova.website</p>
+    <button>Email</button>
+    <button>Linkedin</button>
+  </div>
+}
+
+export default Header
