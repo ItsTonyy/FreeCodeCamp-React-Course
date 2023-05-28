5a1d120a5c8be3cfeda9affa1fe0bd4dae798667
Tony <tonytnportela@outlook.com>
2023-05-28 15:47:00 -0300

    New React Project

---
+++ MemeGenerator/components/header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return(
+    <div className='header'>
+      <img src="/Logo.png" alt="logo-troll-face" className='troll-logo' width={178} height={26}/>
+      <p>React Course - Project 3</p>
+    </div>
+    
+  )
+}
+
+export default Header
---
+++ MemeGenerator/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from '../components/header'
+
+function App() {
+  
+  return (
+    <>
+      <div className="container">
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
